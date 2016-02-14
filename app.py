31e26d18a64999bbaa1ebc58f6456199cbdf57eb
scottjpack <scottjpack@gmail.com>
2016-02-14 15:45:29 -0700

    sample flask app
    sample flask app

---
+++ app.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+from flask import Flask, jsonify, request, abort
+
+app = Flask(__name__)
+
+@app.route('/api/v1.0/add', methods=['POST'])
+def add():
+    if not request.json or not 'x' in request.json or not 'y' in request.json:
+        abort(400)
+    x = request.json.get('x')
+    y = request.json.get('y')
+    return jsonify({'val': x+y})
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
